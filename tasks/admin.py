794611526c191e21e8ebca02cc5a84122bdc216a
mrmoslemi <rmoslemi1994@gmail.com>
2020-02-10 09:31:37 +0330

    Admin class for task model added

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from tasks.models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['creation', 'state', 'scheduled', 'callback', 'execution']
+
+
+admin.site.register(Task, TaskAdmin)
